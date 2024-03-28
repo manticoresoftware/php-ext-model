68ab10766d214740b9f392cefd12f6a3c18d1f52
Don Hardman <don.hardman@muvon.io>
2024-03-28 19:39:44 +0700

    Implement the way to detect input length for a given model and expose those methods to the PHP. It closes #1

--- src/lib.rs
+++ src/lib.rs
@@ -14,8 +14,9 @@ use candle_core::{Device, Tensor};
 use candle_nn::VarBuilder;
 use hf_hub::{api::sync::Api, Repo, RepoType};
 use tokenizers::{Tokenizer, TruncationParams};
+use serde_json::Value;
 
-fn build_model_and_tokenizer(model_id: String, revision: String, use_pth: bool) -> Result<(BertModel, Tokenizer)> {
+fn build_model_and_tokenizer(model_id: String, revision: String, use_pth: bool) -> Result<(BertModel, Tokenizer, usize, usize)> {
   let device = Device::Cpu;
   let repo = Repo::with_revision(model_id, RepoType::Model, revision);
   let (config_filename, tokenizer_filename, weights_filename) = {
@@ -31,6 +32,8 @@ fn build_model_and_tokenizer(model_id: String, revision: String, use_pth: bool) 
     (config, tokenizer, weights)
   };
   let config = std::fs::read_to_string(config_filename)?;
+  let max_input_len = get_max_input_length(&config)?;
+  let hidden_size = get_hidden_size(&config)?;
   let mut config: Config = serde_json::from_str(&config)?;
   let tokenizer: Tokenizer = Tokenizer::from_file(tokenizer_filename)
 		.map_err(E::msg)?;
@@ -42,16 +45,35 @@ fn build_model_and_tokenizer(model_id: String, revision: String, use_pth: bool) 
   };
   config.hidden_act = HiddenAct::GeluApproximate;
   let model = BertModel::load(vb, &config)?;
-  Ok((model, tokenizer))
+  Ok((model, tokenizer, max_input_len, hidden_size))
+}
+
+/// Get maximum input length for sequence for the current model
+fn get_max_input_length(contents: &str) -> Result<usize> {
+  let config: Value = serde_json::from_str(&contents)?;
+  let max_length = config["max_position_embeddings"].as_u64().ok_or_else(|| {
+    std::io::Error::new(std::io::ErrorKind::Other, "Max position embeddings not found")
+  })?;
+  Ok(max_length as usize)
+}
+
+fn get_hidden_size(contents: &str) -> Result<usize> {
+  let config: Value = serde_json::from_str(&contents)?;
+  let max_length = config["hidden_size"].as_u64().ok_or_else(|| {
+    std::io::Error::new(std::io::ErrorKind::Other, "Hidden size not found")
+  })?;
+  Ok(max_length as usize)
 }
 
 #[php_class(name = "Manticore\\Ext\\Model")]
 struct Model {
 	model: BertModel,
 	tokenizer: Tokenizer,
+  max_input_len: usize,
+  hidden_size: usize,
 }
 
-#[php_impl]
+#[php_impl(rename_methods = "camelCase")]
 impl Model {
   /// Static method to instantiate the Tokenizer
   #[php_static_method]
@@ -62,11 +84,26 @@ impl Model {
   pub fn create(model_id: String, revision: Option<String>, use_pth: Option<bool>) -> Self {
   	let revision = revision.unwrap_or("main".to_string());
   	let use_pth = use_pth.unwrap_or(false);
-  	let (model, tokenizer) = build_model_and_tokenizer(model_id, revision, use_pth).unwrap();
+  	let (model, tokenizer, max_input_len, hidden_size) = build_model_and_tokenizer(model_id, revision, use_pth).unwrap();
     Model {
     	model,
-    	tokenizer
+    	tokenizer,
+      max_input_len,
+      hidden_size
     }
+  }
+
+  /// Get maximum input len in tokens allowed for this model
+  /// @return int
+  #[php]
+  pub fn get_max_input_len(&mut self) -> usize {
+    self.max_input_len
+  }
+
+  /// Get resulting vector size for embeddings for this model
+  /// @return int
+  pub fn get_hidde_size(&mut self) -> usize {
+    self.hidden_size
   }
 
   /// @param string $text Text to convert into the token and return array of it
@@ -74,7 +111,7 @@ impl Model {
 	pub fn predict(&mut self, text: String) -> Vec<f32> {
 		let device = &self.model.device;
 		let mut truncation = TruncationParams::default();
-		truncation.max_length = 128;
+		truncation.max_length = self.max_input_len;
 		truncation.strategy = tokenizers::tokenizer::TruncationStrategy::LongestFirst;
   	let tokenizer = self.tokenizer
 	    .with_padding(None)
@@ -93,7 +130,6 @@ impl Model {
 		ys_vec
 	}
 }
-
 
 /// Module initialization
 #[php_module]
