2d6dccd88326cacb7e868a3fc394b0b052de2550
Don Hardman <don.hardman@muvon.io>
2024-04-06 16:26:01 +0700

    Add normalization to fix issue with cosine similiarity

--- src/lib.rs
+++ src/lib.rs
@@ -7,132 +7,154 @@ extern crate intel_mkl_src;
 #[cfg(feature = "accelerate")]
 extern crate accelerate_src;
 
-use candle_transformers::models::bert::{BertModel, HiddenAct, Config, DTYPE};
+use candle_transformers::models::bert::{BertModel, Config, HiddenAct, DTYPE};
 
 use anyhow::{Error as E, Result};
 use candle_core::{Device, Tensor};
 use candle_nn::VarBuilder;
 use hf_hub::{api::sync::Api, Repo, RepoType};
-use tokenizers::{Tokenizer, TruncationParams};
 use serde_json::Value;
+use tokenizers::{Tokenizer, TruncationParams};
 
-fn build_model_and_tokenizer(model_id: String, revision: String, use_pth: bool) -> Result<(BertModel, Tokenizer, usize, usize)> {
-  let device = Device::Cpu;
-  let repo = Repo::with_revision(model_id, RepoType::Model, revision);
-  let (config_filename, tokenizer_filename, weights_filename) = {
-    let api = Api::new()?;
-    let api = api.repo(repo);
-    let config = api.get("config.json")?;
-    let tokenizer = api.get("tokenizer.json")?;
-    let weights = if use_pth {
-      api.get("pytorch_model.bin")?
-    } else {
-      api.get("model.safetensors")?
+fn build_model_and_tokenizer(
+    model_id: String,
+    revision: String,
+    use_pth: bool,
+) -> Result<(BertModel, Tokenizer, usize, usize)> {
+    let device = Device::Cpu;
+    let repo = Repo::with_revision(model_id, RepoType::Model, revision);
+    let (config_filename, tokenizer_filename, weights_filename) = {
+        let api = Api::new()?;
+        let api = api.repo(repo);
+        let config = api.get("config.json")?;
+        let tokenizer = api.get("tokenizer.json")?;
+        let weights = if use_pth {
+            api.get("pytorch_model.bin")?
+        } else {
+            api.get("model.safetensors")?
+        };
+        (config, tokenizer, weights)
     };
-    (config, tokenizer, weights)
-  };
-  let config = std::fs::read_to_string(config_filename)?;
-  let max_input_len = get_max_input_length(&config)?;
-  let hidden_size = get_hidden_size(&config)?;
-  let mut config: Config = serde_json::from_str(&config)?;
-  let tokenizer: Tokenizer = Tokenizer::from_file(tokenizer_filename)
-		.map_err(E::msg)?;
+    let config = std::fs::read_to_string(config_filename)?;
+    let max_input_len = get_max_input_length(&config)?;
+    let hidden_size = get_hidden_size(&config)?;
+    let mut config: Config = serde_json::from_str(&config)?;
+    let tokenizer: Tokenizer = Tokenizer::from_file(tokenizer_filename).map_err(E::msg)?;
 
-  let vb = if use_pth {
-    VarBuilder::from_pth(&weights_filename, DTYPE, &device)?
-  } else {
-    unsafe { VarBuilder::from_mmaped_safetensors(&[weights_filename], DTYPE, &device)? }
-  };
-  config.hidden_act = HiddenAct::GeluApproximate;
-  let model = BertModel::load(vb, &config)?;
-  Ok((model, tokenizer, max_input_len, hidden_size))
+    let vb = if use_pth {
+        VarBuilder::from_pth(&weights_filename, DTYPE, &device)?
+    } else {
+        unsafe { VarBuilder::from_mmaped_safetensors(&[weights_filename], DTYPE, &device)? }
+    };
+    config.hidden_act = HiddenAct::GeluApproximate;
+    let model = BertModel::load(vb, &config)?;
+    Ok((model, tokenizer, max_input_len, hidden_size))
 }
 
 /// Get maximum input length for sequence for the current model
 fn get_max_input_length(contents: &str) -> Result<usize> {
-  let config: Value = serde_json::from_str(&contents)?;
-  let max_length = config["max_position_embeddings"].as_u64().ok_or_else(|| {
-    std::io::Error::new(std::io::ErrorKind::Other, "Max position embeddings not found")
-  })?;
-  Ok(max_length as usize)
+    let config: Value = serde_json::from_str(&contents)?;
+    let max_length = config["max_position_embeddings"]
+        .as_u64()
+        .ok_or_else(|| std::io::Error::new(std::io::ErrorKind::Other, "Max position embeddings not found"))?;
+    Ok(max_length as usize)
 }
 
 fn get_hidden_size(contents: &str) -> Result<usize> {
-  let config: Value = serde_json::from_str(&contents)?;
-  let max_length = config["hidden_size"].as_u64().ok_or_else(|| {
-    std::io::Error::new(std::io::ErrorKind::Other, "Hidden size not found")
-  })?;
-  Ok(max_length as usize)
+    let config: Value = serde_json::from_str(&contents)?;
+    let max_length = config["hidden_size"]
+        .as_u64()
+        .ok_or_else(|| std::io::Error::new(std::io::ErrorKind::Other, "Hidden size not found"))?;
+    Ok(max_length as usize)
 }
 
 #[php_class(name = "Manticore\\Ext\\Model")]
 struct Model {
-	model: BertModel,
-	tokenizer: Tokenizer,
-  max_input_len: usize,
-  hidden_size: usize,
+    model: BertModel,
+    tokenizer: Tokenizer,
+    max_input_len: usize,
+    hidden_size: usize,
 }
 
 #[php_impl(rename_methods = "camelCase")]
 impl Model {
-  /// Static method to instantiate the Tokenizer
-  #[php_static_method]
-  /// @param string $model_id name of the model to use from the huggingface.co
-  /// @param ?string $revision The revision of the mode to use, default is string
-  /// @param bool $use_pth If we use pytorch model or safetensors
-  /// @return self Instance of created class
-  pub fn create(model_id: String, revision: Option<String>, use_pth: Option<bool>) -> Self {
-  	let revision = revision.unwrap_or("main".to_string());
-  	let use_pth = use_pth.unwrap_or(false);
-  	let (model, tokenizer, max_input_len, hidden_size) = build_model_and_tokenizer(model_id, revision, use_pth).unwrap();
-    Model {
-    	model,
-    	tokenizer,
-      max_input_len,
-      hidden_size
+    /// Static method to instantiate the Tokenizer
+    #[php_static_method]
+    /// @param string $model_id name of the model to use from the huggingface.co
+    /// @param ?string $revision The revision of the mode to use, default is string
+    /// @param bool $use_pth If we use pytorch model or safetensors
+    /// @return self Instance of created class
+    pub fn create(model_id: String, revision: Option<String>, use_pth: Option<bool>) -> Self {
+        let revision = revision.unwrap_or("main".to_string());
+        let use_pth = use_pth.unwrap_or(false);
+        let (model, tokenizer, max_input_len, hidden_size) =
+            build_model_and_tokenizer(model_id, revision, use_pth).unwrap();
+        Model {
+            model,
+            tokenizer,
+            max_input_len,
+            hidden_size,
+        }
     }
-  }
 
-  /// Get maximum input len in tokens allowed for this model
-  /// @return int
-  #[php]
-  pub fn get_max_input_len(&mut self) -> usize {
-    self.max_input_len
-  }
+    /// Get maximum input len in tokens allowed for this model
+    /// @return int
+    #[php]
+    pub fn get_max_input_len(&mut self) -> usize {
+        self.max_input_len
+    }
 
-  /// Get resulting vector size for embeddings for this model
-  /// @return int
-  pub fn get_hidde_size(&mut self) -> usize {
-    self.hidden_size
-  }
+    /// Get resulting vector size for embeddings for this model
+    /// @return int
+    pub fn get_hidde_size(&mut self) -> usize {
+        self.hidden_size
+    }
 
-  /// @param string $text Text to convert into the token and return array of it
-  /// @return array<string>
-	pub fn predict(&mut self, text: String) -> Vec<f32> {
-		let device = &self.model.device;
-		let mut truncation = TruncationParams::default();
-		truncation.max_length = self.max_input_len;
-		truncation.strategy = tokenizers::tokenizer::TruncationStrategy::LongestFirst;
-  	let tokenizer = self.tokenizer
-	    .with_padding(None)
-	    .with_truncation(Some(truncation))
-	    .map_err(E::msg).unwrap();
-		let tokens = tokenizer
-			.encode(text.clone(), true)
-			.map_err(E::msg).unwrap()
-			.get_ids()
-			.to_vec();
-		let token_ids = Tensor::new(&tokens[..], device).unwrap().unsqueeze(0).unwrap();
-		let token_type_ids = token_ids.zeros_like().unwrap();
-		let ys = self.model.forward(&token_ids, &token_type_ids).unwrap();
-		let cls_embedding = ys.get(0).unwrap().get(0).unwrap();
-		let ys_vec = cls_embedding.flatten_all().unwrap().to_vec1().unwrap();
-		ys_vec
-	}
+    /// @param string $text Text to convert into the token and return array of it
+    /// @return array<string>
+    pub fn predict(&mut self, text: String) -> Vec<f32> {
+        let device = &self.model.device;
+        let mut truncation = TruncationParams::default();
+        truncation.max_length = self.max_input_len;
+        truncation.strategy = tokenizers::tokenizer::TruncationStrategy::LongestFirst;
+        let tokenizer = self
+            .tokenizer
+            .with_padding(None)
+            .with_truncation(Some(truncation))
+            .map_err(E::msg)
+            .unwrap();
+        let tokens = tokenizer
+            .encode(text.clone(), true)
+            .map_err(E::msg)
+            .unwrap()
+            .get_ids()
+            .to_vec();
+        let token_ids = Tensor::new(&tokens[..], device).unwrap().unsqueeze(0).unwrap();
+        let token_type_ids = token_ids.zeros_like().unwrap();
+        let embeddings = self.model.forward(&token_ids, &token_type_ids).unwrap();
+
+        // Apply some avg-pooling by taking the mean embedding value for all tokens (including padding)
+        let (n_sentences, n_tokens, _hidden_size) = embeddings.dims3().unwrap();
+        let embeddings = (embeddings.sum(1).unwrap() / (n_tokens as f64)).unwrap();
+
+        let mut results: Vec<Vec<f32>> = Vec::new();
+        for j in 0..n_sentences {
+            let e_j = embeddings.get(j).unwrap();
+            let mut emb: Vec<f32> = e_j.to_vec1().unwrap();
+            normalize(&mut emb);
+            results.push(emb);
+        }
+        results.get(0).unwrap().clone()
+    }
 }
 
 /// Module initialization
 #[php_module]
 pub fn get_module(module: ModuleBuilder) -> ModuleBuilder {
-	module
+    module
+}
+
+pub fn normalize(v: &mut Vec<f32>) {
+    let length: f32 = v.iter().map(|x| x * x).sum::<f32>().sqrt();
+    v.iter_mut().for_each(|x| *x /= length);
 }
